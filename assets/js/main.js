9952e04a4c3417c26943dcc02c04edfec3c74283
kradhenz <tomas25498@gmail.com>
2024-02-06 20:58:12 -0300

    :construction: function toNumber added :construction:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+function toNumber(human) {
+    if (human == 'Piedra' || human == 'piedra' || human == 'PIEDRA') {
+        return 1;
+    } else if (human == 'Papel' || human == 'papel' || human == 'PAPEL') {
+        return 2;
+    } else if (human == 'Tijera' || human == 'tijera' || human == 'TIJERA') {
+        return 3;
+    } else {
+        return false;
+    }
+}
+
 let num = prompt('Cuántas veces desea jugar?');
 for (let i = 0; i < num; i++) {
     let human = prompt('¿Piedra, Papel o Tijera?')
