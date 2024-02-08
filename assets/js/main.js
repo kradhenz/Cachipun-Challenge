b9fb54966f4162b79aa64e7cfb5e900317eccaf4
kradhenz <tomas25498@gmail.com>
2024-02-08 16:34:06 -0300

    :bug: anti break up code added :ok_hand:

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,19 +63,33 @@ function toNumber(human) {
 
 /* START
 Request a number of time(s) to play to a human player */
-let num = prompt('Cuántas veces desea jugar?');
-for (let i = 0; i < num; i++) {
-    // Request an option to a human player (and showing the game's number)
-    let human = prompt(`JUEGO Nro. ${i+1} \n¿Piedra, Papel o Tijera?`)
-    // Conditional statement to limit the options in the game
-    if (toNumber(human) == false) {
-        alert('¡LA OPCIÓN NO ES VÁLIDA!\n Las opciones válidas son:\n > Piedra, piedra, PIEDRA o \n > Papel, papel, PAPEL o \n > Tijera, tijera, TIJERA');
-    } else {
-        // Generate a random between 1 and 3 to a machine player
-        let random = Math.floor(Math.random() * 3) + 1;
-        // Show the versus players
-        alert(`Tú: ${human} \nVS \nMáquina: ${toString(random)}`);
-        // Deliver result of game
-        result(toNumber(human), random);
+let num;
+do {
+    num = +prompt('Cuántas veces desea jugar?');
+    for (let i = 0; i < num; i++) {
+        let human;
+        do {
+            // Request an option to a human player (and showing the game's number)
+            human = prompt(`JUEGO Nro. ${i + 1} \n¿Piedra, Papel o Tijera?`)
+            // Conditional statement to limit the options in the game
+            if (toNumber(human) == false) {
+                alert('¡LA OPCIÓN NO ES VÁLIDA!\n Las opciones válidas son:\n > Piedra, piedra, PIEDRA o \n > Papel, papel, PAPEL o \n > Tijera, tijera, TIJERA');
+            } else {
+                // Generate a random between 1 and 3 to a machine player
+                let random = Math.floor(Math.random() * 3) + 1;
+                // Show the versus players
+                alert(`Tú: ${human} \nVS \nMáquina: ${toString(random)}`);
+                // Deliver result of the game
+                result(toNumber(human), random);
+            }
+        } while (toNumber(human) == false);
     }
-}
+    // Verify if 'num' variable is an Integer
+    if (Number.isInteger(num) == false || num < 0) {
+        alert(`El número o caracter ingresado no es válido. No se ha podido iniciar el juego.\nPor favor inténtelo nuevamente`);
+    } else if (num == 0) {
+        alert('La máquina tampoco quería jugar.')
+    } else {
+        alert('EL JUEGO HA TERMINADO')
+    }
+} while (Number.isInteger(num) == false || num < 0);
